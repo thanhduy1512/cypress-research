0f973fe9b510ad6bcefe10d98c8e3289a4e36e07
Duong Thanh Duy <duongduyy1512@gmail.com>
2023-06-28 18:03:22 +0700

    add ui for shows page

--- next-shows/pages/shows/index.tsx
+++ next-shows/pages/shows/index.tsx
@@ -1,4 +1,27 @@
+import Link from 'next/link';
+import styles from '../../styles/Shows.module.css';
+
 const Shows = () => {
-  return <div className="container">Shows</div>;
+  return (
+    <div className={styles.showContainer + ' ' + 'container'}>
+      <h1>Upcoming Shows</h1>
+      <ul>
+        <li>
+          <p>Date</p>
+          <Link href="/">
+            <button>Tickets</button>
+          </Link>
+          <p>Description</p>
+        </li>
+        <li>
+          <p>Date</p>
+          <Link href="/">
+            <button>Tickets</button>
+          </Link>
+          <p>Description</p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Shows;
