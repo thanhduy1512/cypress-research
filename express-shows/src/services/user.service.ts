b09b28b44dc1a8f375d8c7c67bea48bc1951d103
Duong Thanh Duy <duongduyy1512@gmail.com>
2023-07-03 15:38:54 +0700

    add bcrypt to save hash password

--- express-shows/src/services/user.service.ts
+++ express-shows/src/services/user.service.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import { AppDataSource } from '../database/AppDataSource';
 import { User } from '../entities/user.entity';
 
@@ -8,7 +9,10 @@ const createUser = async (req: Request, res: Response) => {
   const { username, password } = req.body;
   const user = new User();
   user.username = username;
-  user.password = password;
+  bcrypt.hash(password, process.env.SALT_ROUNDS || 10, (err, hash) => {
+    if (err) return res.status(500).json({ message: 'Error password' });
+    user.password = hash;
+  });
 
   await userRepository.save(user);
   return res.status(201).json(user);
@@ -26,10 +30,18 @@ const getUserById = async (req: Request, res: Response) => {
 };
 
 const updateUser = async (req: Request, res: Response) => {
-  // const userFound = await userRepository.findBy({ username: 'dex' });
   const id = Number(req.params.id);
+  const { password } = req.body;
+
   const userFound = await userRepository.findOne({ where: { id } });
   //update user
+  if (!userFound) return res.status(404).json({ message: 'User not found' });
+
+  bcrypt.hash(password, process.env.SALT_ROUNDS || 10, (err, hash) => {
+    if (err) return res.status(500).json({ message: 'Error password' });
+    userFound.password = hash;
+  });
+  await userRepository.save(userFound);
   return res.status(200).json(userFound);
 };
 
@@ -38,6 +50,15 @@ const deleteUser = async (req: Request, res: Response) => {
   const id = Number(req.params.id);
   await userRepository.delete({ id });
   return res.status(201).json();
+};
+
+const authenticate = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await userRepository.findOne({ where: { username } });
+  if (user && bcrypt.compareSync(password, user.password)) {
+    return res.status(200).json(user);
+  }
+  return res.status(401).json({ message: 'Unauthorized' });
 };
 
 export { createUser, getAllUser, getUserById, updateUser, deleteUser };
