6d26fddf712943fea74e3717f85ab2840508b1db
Duong Thanh Duy <duongduyy1512@gmail.com>
2023-07-18 11:13:52 +0700

    add form signin UI

--- next-shows/pages/signin/index.tsx
+++ next-shows/pages/signin/index.tsx
@@ -1,4 +1,32 @@
+import { FormEvent, useRef } from 'react';
+import styles from './Sigin.module.css';
+
 const Signin = () => {
-  return <div className="container">Signin</div>;
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const onSubmitSigin = (e: FormEvent) => {
+    e.preventDefault();
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+    console.log(username);
+    console.log(password);
+  };
+
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.contentContainer}>
+        <h1 className={styles.title}>Signin to your account</h1>
+        <form onSubmit={onSubmitSigin} className={styles.modalContainer}>
+          <label>Username</label>
+          <input type="text" ref={usernameRef} />
+          <label htmlFor="password">Password</label>
+          <input type="password" ref={passwordRef} />
+
+          <button type="submit">Signin</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Signin;
